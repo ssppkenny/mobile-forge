8d50ea74a52f28cd9ecf4ea51cdd9a779e705f61
Sergey Mikhno <sergey.mikhno@gmail.com>
2026-05-03 15:01:39 +0200

    Remove .ci

--- .ci/common.sh
+++
@@ -1,7 +0,0 @@
-function publish_to_pypi() {
-    if [[ "$APPVEYOR_PULL_REQUEST_NUMBER" == "" ]]; then
-        for wheel in "$@"; do
-            curl -F package=@$wheel https://$GEMFURY_TOKEN@push.fury.io/flet/
-        done
-    fi
-}
